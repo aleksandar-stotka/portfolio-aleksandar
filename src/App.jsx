f90dbcccd15008166805f5ec307caca65edbaccc
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-09-09 18:03:52 -0700

    set-about-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import Hero from './Hero';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import About from './components/About';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
     <Hero/>
-  
+    <Skills/>
+   <About/>
     </div>
   );
 }
