83a1563072b5c40f6c99f6ceada25204bf957226
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-09-09 18:07:43 -0700

    components-done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import About from './components/About';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Hero/>
     <Skills/>
    <About/>
+   <Projects/>
     </div>
   );
 }
