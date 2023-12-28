018eee32c6c24caf220b0d397680648462a8514d
aleksandar-stotka <aangelevski6@gmail.com>
2023-12-28 14:49:17 +0100

    alt-set

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className="bg-white py-20" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
-        <img src={aboutSvg} className="w-full h-64" />
+        <img src={aboutSvg} className="w-full h-64" alt="img" />
         <article>
           <SectionTitle text="code and coffee" />
           <p className="text-slate-600 mt-8 leading-loose">
