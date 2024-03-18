14d2f8d5abe63e92ad66865a98b10c5770606c82
aleksandar-stotka <aangelevski6@gmail.com>
2024-03-18 06:55:10 +0000

    text_about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,8 @@ const About = () => {
           <SectionTitle text="code and coffee" />
           <p className="text-slate-600 mt-8 leading-loose">
 
-            Greetings! I'm Aleksandar, a Front-End Developer from Macedonia, driven by a passion for coding and innovation. My journey into the world of web development began with a focus on Front-End languages, and I've since broadened my skill set to include various DevOps segments. Through continuous learning and exploration, I've enriched my knowledge and expertise, constantly striving to refine my craft. My GitHub profile showcases projects that demonstrate my proficiency in JavaScript, among other languages. I'm excited about the opportunity to contribute my skills and dedication to your team and organization, and I'm eager to explore new opportunities within your company.
 
+            Greetings! I'm Aleksandar, a Front-End Developer from Macedonia, driven by a passion for coding and innovation. My journey into the world of web development began with a focus on Front-End languages, and I've since broadened my skill set to include various DevOps segments. Through continuous learning and exploration, I've enriched my knowledge and expertise, constantly striving to refine my craft. My GitHub profile showcases projects that demonstrate my proficiency in JavaScript, among other languages. I'm excited about the opportunity to contribute my skills and dedication to new projects and teams, and I'm eager to explore fresh challenges and opportunities in the field
           </p>
         </article>
       </div>
